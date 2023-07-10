481cf6b7ce82389153af09bf9c7dddf1c4c8cff6
kisyular <rellika@kisyula.com>
2023-07-10 13:07:56 +0300

    LEARNING NEXT-JS - NAVBAR

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import '@styles/globals.css'
+import Nav from '@components/Nav'
 
 export const metadata = {
 	title: 'Prompts',
@@ -12,7 +13,10 @@ const RootLayout = ({ children }) => (
 				<div className='gradient' />
 			</div>
 
-			<main className='app'>{children}</main>
+			<main className='app'>
+				<Nav />
+				{children}
+			</main>
 		</body>
 	</html>
 )
