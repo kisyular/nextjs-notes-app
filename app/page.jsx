e60f0c220c9dca28e40304b20499065e7f20160e
kisyular <rellika@kisyula.com>
2023-07-14 14:28:08 +0300

    NEXT-JS - READING PROMPTS FROM MONGODB

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from '@components/Feed'
 const Home = () => {
 	return (
 		<section className='w-full flex-center flex-col'>
@@ -14,7 +15,7 @@ const Home = () => {
 				to discover, create and share creative prompts
 			</p>
 
-			{/* <Feed /> */}
+			<Feed />
 		</section>
 	)
 }
